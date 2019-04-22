43df00deae34cb00ae49952909e4edce27450f49
yzhukovs <yzhukovs@bumnetworks.com>
2019-04-22 17:09:14 -0400

    write some code

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,7 +10,18 @@
 */
 int fizzbuzz(int n)
 {
+    if ((n%15) == 0 )
+    printf("Fizzbuzz\n");
 
+    else if ((n%3) == 0 )
+      printf("Fizz\n");
+
+    else if ((n%5) == 0 )
+        printf("Buzz\n");
+    else
+        printf("%d\n", n);
+    
+    return 0;
 }
 
 #ifndef TESTING
